9f076a186b0c16474a4c7f2d938f55dec4e1009d
Chandra Mohan Rayaprol <45407360+Raysapps@users.noreply.github.com>
2023-05-08 14:19:52 +1000

    Add project files.

---
+++ PnpIndexModel.cs
@@ -0,0 +1,63 @@
+﻿// Copyright (c) 2023  RAYSAPPS PTY LTD Licensed under the MIT License.
+//  See LICENSE in the project root for license information.
+//  This file is strictly confidential and internal to the company.
+
+using System.Text.Json;
+
+namespace RaysApps.Services.PlugAndPlayFormat;
+
+public record RaysIndexModel(string Name, string? DisplayName, string? Description, IReadOnlyDictionary<string, string>? DisplayNames, IReadOnlyDictionary<string, string>? DescriptionNames);
+public class PnPIndexModel
+{
+    public static RaysIndexModel GetPnPIndexModel(JsonProperty prop)
+    {
+        _ = setProperty(prop.Value, "displayName", out var displayName, out var displayNamesVal);
+        _ = setProperty(prop.Value, "description", out var description, out var discriptionNamesVal);
+        return new RaysIndexModel(prop.Name, displayName, description, displayNamesVal, discriptionNamesVal);
+    }
+
+    private static readonly string[] s_engLang = new[] { "en", "en-au", "en-bz", "en-ca", "en-cb", "en-gb", "en-in", "en-ie", "en-jm","en-nz", "en-ph",
+    "en-za","en-tt", "en-us"};
+    private static bool setProperty(JsonElement model, string propTypeName, out string? propStringVal, out Dictionary<string, string> propDicVal)
+    {
+        propStringVal = null;
+        propDicVal = new();
+        if (!model.TryGetProperty(propTypeName, out var propTypeNameValue))
+        {
+            return false;
+        }
+
+        switch (propTypeNameValue.ValueKind)
+        {
+            case JsonValueKind.String:
+                {
+                    propStringVal = propTypeNameValue.GetString();
+                    return true;
+                }
+            case JsonValueKind.Object:
+                {
+
+                    foreach (var item in propTypeNameValue.EnumerateObject())
+                    {
+                        // default first property value
+                        if (string.IsNullOrEmpty(propStringVal))
+                        {
+                            propStringVal = item.Value.ToString();
+                        }
+                        // Add display Names to the Array.
+
+                        var val = item.Value.ToString();
+                        propDicVal.Add(item.Name, val);
+                        // check for available english language and assign it to the display name. 
+                        if (s_engLang.Contains(item.Name.ToLower()))
+                        {
+                            propStringVal = item.Value.GetString();
+                        }
+                    }
+                    return true;
+                }
+            default: return false;
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RaysApps.Services.PlugAndPlayFormat;
+using System.Net.Http.Json;
+using System.Text.Json;
+Dictionary<string, RaysIndexModel> _indexKeyModels = new();
+List<RaysIndexModel> _indexModels = new();
+HttpClient httpClient = new();
+var resp = await httpClient.GetFromJsonAsync<JsonElement>("https://devicemodels.azure.com/index.json");
+var models = resp.GetProperty("models");
+foreach (var model in models.EnumerateObject())
+{
+    var indexModel = PnPIndexModel.GetPnPIndexModel(model);
+    _indexKeyModels.Add(model.Name, indexModel);
+    _indexModels.Add(indexModel);
+}
+int i = 1;
+foreach (var model in _indexKeyModels)
+{
+    Console.WriteLine($"{i++} -->  {model.Key}");
+    Console.WriteLine($"DisplayName: {model.Value.DisplayName}");
+}
+   
